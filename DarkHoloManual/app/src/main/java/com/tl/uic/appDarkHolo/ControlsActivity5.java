02081a2044b3925c196060399afa32f67a1b430c
Changjie Yang <cyang@us.ibm.com>
2018-11-30 14:02:01 -0800

    Cherry Picked from betaLegacy28 for AndroidX build.

--- DarkHoloManual/app/src/main/java/com/tl/uic/appDarkHolo/ControlsActivity5.java
+++ DarkHoloManual/app/src/main/java/com/tl/uic/appDarkHolo/ControlsActivity5.java
@@ -16,41 +16,49 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
+import com.tl.uic.Tealeaf;
+
 public class ControlsActivity5 extends AppCompatActivity {
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.controls5);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.controls5);
 
-		Button button = (Button) findViewById(R.id.button1);
-		button.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Builder builder = new Builder(v.getContext());
-				String title = getString(R.string.alertTitle);
-				String message = getString(R.string.alertMessage1);
-				builder.setMessage(message)
-						.setTitle(title)
-						.setCancelable(false)
-						.setNegativeButton("OK",
-								new DialogInterface.OnClickListener() {
-									public void onClick(DialogInterface dialog, int id) {
-										dialog.cancel();
-									}
-								});
-				final AlertDialog dialog = builder.create();
-				dialog.show();
-			}
-		});
+        Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Tealeaf.logEvent(v);
+                Builder builder = new Builder(v.getContext());
+                String title = getString(R.string.alertTitle);
+                String message = getString(R.string.alertMessage1);
+                builder.setMessage(message)
+                        .setTitle(title)
+                        .setCancelable(false)
+                        .setNegativeButton("OK",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        Tealeaf.logDialogEvent(dialog, id);
+                                        dialog.cancel();
+                                        Tealeaf.logScreenLayout(ControlsActivity5.this.getParent(), "CA5", 500);
+                                    }
+                                });
+                final AlertDialog dialog = builder.create();
+                Tealeaf.logScreenLayoutSetOnShowListener(ControlsActivity5.this.getParent(), dialog);
+                dialog.show();
+            }
+        });
 
-		button = (Button) findViewById(R.id.button2);
-		button.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				FragmentManager fm = getSupportFragmentManager();
-				String message = getString(R.string.alertMessage2);
-				MyDialogFragment alertDialog = MyDialogFragment.newInstance(message);
-				alertDialog.show(fm, "fragment_alert");
-			}
-		});
-	}
+        button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Tealeaf.logEvent(v);
+
+                FragmentManager fm = getSupportFragmentManager();
+                String message = getString(R.string.alertMessage2);
+                MyDialogFragment alertDialog = MyDialogFragment.newInstance(message);
+                alertDialog.show(fm, "fragment_alert");
+            }
+        });
+    }
 }
